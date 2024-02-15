9d2440af1f4a4c0f0c3bd3f91febeb0e1b0a2d7d
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-02-15 19:00:02 -0300

    added typer effect

---
+++ script.ts
@@ -0,0 +1,32 @@
+const typer =  document.querySelector('.typing')
+const text1 = 'A aspiring front-end dev';
+const text2 = 'A linux entusiast'
+const text3 = 'Open source contributor'
+
+function typingEffect(element, text1, i = 0) {
+     if (i === 0) {
+         element.textContent = ""
+     }
+    element.textContent += text1[i];
+    
+    if (i === text1.length - 1) {
+        return;
+    }
+
+    setTimeout(() => typingEffect(typer, text1, i + 1), 30)
+    
+    if (element.textContent = "A aspiring front-end dev") {
+        setTimeout(() => eraserEffect, 30)
+    }
+    
+    // setTimeout(() => typingEffect(typer, text2, i + 1), 30)
+
+    
+}
+
+typingEffect(typer, text1, 0);
+
+
+function eraserEffect() {
+  textContent.slice(0,-1)
+}
