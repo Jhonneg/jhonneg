5d4e5c159dcae9f0139d70c6eaa22a6f1c8b4ff4
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-08-18 07:17:57 -0300

    main portfolio done

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,19 +9,24 @@ export default function ProjectCard({
 }: ProjectCardProps) {
   return (
     <a href={anchorLink}>
-      <div className="card card-normal bg-base-100 shadow-xl">
-        <figure>
-          <img src={mediaUrl} alt={name} />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p>{description}</p>
-          <div className="card-actions justify-center">
-            {techStack.map((tech, index) => (
-              <div key={index} className="badge badge-outline">
-                {tech}
-              </div>
-            ))}
+      <div>
+        <div
+          className="hero min-h-80"
+          style={{
+            backgroundImage: `url(${mediaUrl})`,
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-neutral-content text-center">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">{name}</h1>
+              <p className="mb-5">{description}</p>
+              {techStack.map((tech, index) => (
+                <div key={index} className="badge badge-primary">
+                  {tech}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- src/components/ProjectGallery.tsx
+++ src/components/ProjectGallery.tsx
@@ -6,18 +6,18 @@ export default function ProjectGallery() {
     <>
       <div>
         <p className="text-4xl">Projects</p>
-      </div>
-      <div className="gap-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 my-12">
-        {projectsData.map((projects) => (
-          <ProjectCard
-            key={projects.name}
-            name={projects.name}
-            techStack={projects.techStack}
-            description={projects.description}
-            anchorLink={projects.anchorLink}
-            mediaUrl={projects.mediaUrl}
-          />
-        ))}
+        <div className="gap-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 my-12">
+          {projectsData.map((projects) => (
+            <ProjectCard
+              key={projects.name}
+              name={projects.name}
+              techStack={projects.techStack}
+              description={projects.description}
+              anchorLink={projects.anchorLink}
+              mediaUrl={projects.mediaUrl}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
